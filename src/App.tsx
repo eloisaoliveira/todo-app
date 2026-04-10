a9b642aa3fc8e3211ade66842579538f3b8bfe44
eloisaoliveira <eloisa.oliveirasantos@gmail.com>
2026-04-09 22:34:22 -0300

    :sparkles: feat: text component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import Text from "./components/text";
 
 export default function App() {
 
-  return <>Olá Mundo!</>
+  return (
+    <div className="flex flex-col gap-2">
+      <Text variant="body-sm-bold" className="text-pink-base">
+        Olá mundo!
+      </Text>
+      <Text className="text-green-base">Olá mundo!</Text>
+      <Text variant="body-md-bold">Olá mundo!</Text>
+    </div>
+  )
 }
