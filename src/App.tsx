ef4123dd6c29ae63da3a5b780ce2ad769933aac1
eloisaoliveira <eloisa.oliveirasantos@gmail.com>
2026-04-10 15:24:25 -0300

    :sparkles: feat: button component

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import PlusIcon from "./assets/icons/plus.svg?react";
 import SpinnerIcon from "./assets/icons/spinner.svg?react";
 import Icon from "./components/icon";
 import Badge from "./components/badge";
+import Button from "./components/button";
 
 export default function App() {
 
@@ -29,12 +30,15 @@ export default function App() {
         <Icon svg={SpinnerIcon} />
         <Icon svg={PencilIcon} />
         <Icon svg={XIcon} />
-
+      </div>
 
       <div>
         <Badge variant="secondary">5</Badge>
         <Badge variant="primary">2 de 5</Badge>
       </div>
+      
+      <div>
+        <Button icon={PlusIcon}>Nova tarefa</Button>
       </div>
     </div>
   )
