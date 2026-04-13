95d8638371aece7e1f519d0cdf51f397cd05c087
eloisaoliveira <eloisa.oliveirasantos@gmail.com>
2026-04-13 17:47:27 -0300

    :sparkles: feat: card component

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import Button from "./components/button";
 import ButtonIcon from "./components/button-icon";
 import InputText from "./components/input-text";
 import InputCheckbox from "./components/input-checkbox";
+import Card from "./components/card";
 
 export default function App() {
 
@@ -56,6 +57,10 @@ export default function App() {
 
        <div>
         <InputCheckbox />
+      </div>
+
+      <div>
+        <Card size="md">Olá mundo</Card>
       </div>
     </div>
   )
